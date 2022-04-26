e2dd0da09ae1bacfa5f11a1bd11a283639b9a0a8
Alexander <a.pride.xxi@gmail.com>
2022-04-26 18:45:05 +0300

    Initial Commit

---
+++ Unsplash Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unsplash Clone
+//
+//  Created by Александр Прайд on 26.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
